04c92b169ef9457cb82b07ead7e3333448cf0c53
Anil171-op <harish30curry@gmail.com>
2026-03-02 11:26:37 +0530

    uc7

--- App1/src/Palindrome.java
+++ App1/src/Palindrome.java
@@ -1,58 +1,30 @@
-// File: UseCase8PalindromeCheckerApp.java
-public class Palindrome {
-    static class Node {
-        char data;
-        Node next;
-        Node(char d) { data = d; }
-    }
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Palindrome {
     public static void main(String[] args) {
         String word = "madam";
-        Node head = buildList(word);
+        Deque<Character> deque = new LinkedList<>();
 
-        if (isPalindrome(head)) {
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i)); // insert at rear
+        }
+
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
         }
-    }
-
-    static Node buildList(String str) {
-        Node head = new Node(str.charAt(0));
-        Node current = head;
-        for (int i = 1; i < str.length(); i++) {
-            current.next = new Node(str.charAt(i));
-            current = current.next;
-        }
-        return head;
-    }
-
-    static boolean isPalindrome(Node head) {
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) return false;
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-        return true;
-    }
-
-    static Node reverse(Node head) {
-        Node prev = null, current = head;
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
     }
 }
